071994bf55ce0de3ff469e954a8584d5967b3196
CharlieAlbert <charlieraph36@gmail.com>
2023-03-20 23:01:27 +0300

    Added a chat message component and manipulated node-modules

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,17 @@ import Footer from "./Components/Footer";
 import Profile from "./Components/Profile";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserDetails from "./Components/UserDetails";
-
+import ChatRoom from "./Components/ChatRoom";
 
 function App() {
   return (
-    <div className="App">   
-      <Navbar />
+    <div className="App">
+      <ChatRoom />
+      {/* <Navbar />
       <Hero />
       <Slider />
       <Contact />
-      <Footer />
-      {/* <UserDetails /> */}
+      <Footer /> */}
     </div>
   );
 }
